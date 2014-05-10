a2dd095a8aa9061197f082cab60a9f917803480b
Matěj Korvas <korvas@ufal.mff.cuni.cz>
2014-05-10 21:58:53 +0200

    Added --{input,output}-dir args to train.py.

--- alex/applications/PublicTransportInfoCS/slu/train.py
+++ alex/applications/PublicTransportInfoCS/slu/train.py
@@ -3,7 +3,12 @@
 
 from __future__ import unicode_literals
 
-import autopath
+import argparse
+import os
+from os.path import exists, join
+
+if __name__ == "__main__":
+    import autopath
 
 from alex.applications.PublicTransportInfoCS.preprocessing import PTICSSLUPreprocessing
 from alex.components.asr.utterance import Utterance, UtteranceNBList
@@ -12,20 +17,42 @@ from alex.components.slu.base import CategoryLabelDatabase
 from alex.components.slu.dailrclassifier import DAILogRegClassifier
 from alex.corpustools.wavaskey import load_wavaskey
 
+###############################################################################
+#                                  Constants                                  #
+###############################################################################
+
+MIN_CLASSIFIER_COUNT = 4
+MIN_FEATURE_COUNT = 3
+
+MODEL_FNAME_TPT = "dailogreg.{utthyp_type}.model"
+
+###############################################################################
+#                                  Functions                                  #
+###############################################################################
+
+
+def get_model_fname(parts, utthyp_type):
+    model_fname = MODEL_FNAME_TPT.format(utthyp_type=utthyp_type)
+    if parts == 'all':
+        model_fname += '.all'
+    return model_fname
+
+
 def increase_weight(d, weight):
     new_d = {}
     for i in range(weight):
         for k in d:
-            new_d["{k}v_{i}".format(k=k,i=i)] = d[k]
+            new_d["{k}v_{i}".format(k=k, i=i)] = d[k]
 
     d.update(new_d)
+
 
 def train(fn_model,
           fn_transcription, constructor, fn_annotation,
           fn_bs_transcription, fn_bs_annotation,
-          min_feature_count = 2,
-          min_classifier_count = 2,
-          limit = 100000):
+          min_feature_count=2,
+          min_classifier_count=2,
+          limit=100000):
     """
     Trains a SLU DAILogRegClassifier model.
 
@@ -36,13 +63,13 @@ def train(fn_model,
     :param limit:
     :return:
     """
-    bs_utterances = load_wavaskey(fn_bs_transcription, Utterance, limit = limit)
-    increase_weight(bs_utterances, min_feature_count+10)
-    bs_das = load_wavaskey(fn_bs_annotation, DialogueAct, limit = limit)
-    increase_weight(bs_das, min_feature_count+10)
+    bs_utterances = load_wavaskey(fn_bs_transcription, Utterance, limit=limit)
+    increase_weight(bs_utterances, min_feature_count + 10)
+    bs_das = load_wavaskey(fn_bs_annotation, DialogueAct, limit=limit)
+    increase_weight(bs_das, min_feature_count + 10)
 
-    utterances = load_wavaskey(fn_transcription, constructor, limit = limit)
-    das = load_wavaskey(fn_annotation, DialogueAct, limit = limit)
+    utterances = load_wavaskey(fn_transcription, constructor, limit=limit)
+    das = load_wavaskey(fn_annotation, DialogueAct, limit=limit)
 
     utterances.update(bs_utterances)
     das.update(bs_das)
@@ -52,33 +79,60 @@ def train(fn_model,
     slu = DAILogRegClassifier(cldb, preprocessing, features_size=4)
 
     slu.extract_classifiers(das, utterances, verbose=True)
-    slu.prune_classifiers(min_classifier_count = min_classifier_count)
+    slu.prune_classifiers(min_classifier_count=min_classifier_count)
     slu.print_classifiers()
     slu.gen_classifiers_data()
-    slu.prune_features(min_feature_count = min_feature_count, verbose=True)
+    slu.prune_features(min_feature_count=min_feature_count, verbose=True)
 
     slu.train(inverse_regularisation=1e1, verbose=True)
 
     slu.save_model(fn_model)
 
-def main():
-    min_classifier_count = 4
-    min_feature_count = 3
 
-    train('./dailogreg.trn.model.all', './all.trn', Utterance,       './all.trn.hdc.sem', './bootstrap.trn', './bootstrap.sem',
-        min_feature_count = min_feature_count, min_classifier_count = min_classifier_count)
-    train('./dailogreg.asr.model.all', './all.asr', Utterance,       './all.trn.hdc.sem', './bootstrap.trn', './bootstrap.sem',
-        min_feature_count = min_feature_count, min_classifier_count = min_classifier_count)
-    train('./dailogreg.nbl.model.all', './all.nbl', UtteranceNBList, './all.trn.hdc.sem', './bootstrap.trn', './bootstrap.sem',
-        min_feature_count = min_feature_count, min_classifier_count = min_classifier_count)
+def parse_args(argv=None):
+    arger = argparse.ArgumentParser(
+        description="Trains a MaxEnt SLU classifier for given training data.")
+    arger.add_argument('-i', '--input-dir',
+                       metavar='DIR',
+                       help='Path towards a directory with all needed '
+                            'training files.')
+    arger.add_argument('-o', '--output-dir',
+                       metavar='DIR',
+                       help='Path towards a directory where trained models '
+                            'shall be written. If the directory does not '
+                            'exist, it will be created.')
 
-    train('./dailogreg.trn.model', './train.trn', Utterance,       './train.trn.hdc.sem', './bootstrap.trn', './bootstrap.sem',
-        min_feature_count = min_feature_count, min_classifier_count = min_classifier_count)
-    train('./dailogreg.asr.model', './train.asr', Utterance,       './train.trn.hdc.sem', './bootstrap.trn', './bootstrap.sem',
-        min_feature_count = min_feature_count, min_classifier_count = min_classifier_count)
-    train('./dailogreg.nbl.model', './train.nbl', UtteranceNBList, './train.trn.hdc.sem', './bootstrap.trn', './bootstrap.sem',
-        min_feature_count = min_feature_count, min_classifier_count = min_classifier_count)
+    args = arger.parse_args(argv)
+    return args
+
+
+def main(argv=None):
+    args = parse_args(argv)
+
+    indir = args.input_dir
+    outdir = args.output_dir
+    exists(outdir) or os.makedirs(outdir)
+
+    for parts in ('all', 'train'):
+        for utthyp_type in ('trn', 'asr', 'nbl'):
+            if utthyp_type in ('trn', 'asr'):
+                utthyp_constructor = Utterance
+            else:
+                utthyp_constructor = UtteranceNBList
+
+            model_fname = get_model_fname(parts, utthyp_type)
+            trs_fname = '{parts}.{utthyp_type}'.format(**locals())
+            sem_fname = '{parts}.{utthyp_type}.hdc.sem'.format(**locals())
+
+            train(join(outdir, model_fname),
+                  join(indir, trs_fname),
+                  utthyp_constructor,
+                  join(indir, sem_fname),
+                  join(indir, 'bootstrap.trn'),
+                  join(indir, 'bootstrap.sem'),
+                  min_feature_count=MIN_FEATURE_COUNT,
+                  min_classifier_count=MIN_CLASSIFIER_COUNT)
 
 
 if __name__ == '__main__':
-  main()
+    main()
