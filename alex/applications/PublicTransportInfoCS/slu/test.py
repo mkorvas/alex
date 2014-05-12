0f38ef9d5e9aace6cb4fa3e42d2cb0391cb8e0c8
Matěj Korvas <korvas@ufal.mff.cuni.cz>
2014-05-12 22:49:29 +0200

    Polished test.py, enabled specifying output dir.

--- alex/applications/PublicTransportInfoCS/slu/test.py
+++ alex/applications/PublicTransportInfoCS/slu/test.py
@@ -13,10 +13,12 @@ if __name__ == "__main__":
 
 # XXX This function should really be defined somewhere else than prepare_data.
 from alex.applications.PublicTransportInfoCS.prepare_data import sort_dais
+from alex.applications.PublicTransportInfoCS.preprocessing import PTICSSLUPreprocessing
 from alex.applications.PublicTransportInfoCS.train import (
     constructor_for_utthyp,
     get_model_fname)
 from alex.components.asr.utterance import Utterance, UtteranceNBList
+from alex.components.slu.base import CategoryLabelDatabase
 
 ###############################################################################
 #                                  Constants                                  #
@@ -31,34 +33,42 @@ PARTS = ('train', 'dev', 'test')
 ###############################################################################
 
 
-def trained_slu_test(fn_model, fn_input, constructor, fn_reference):
+def test_slu(slu, fn_input, constructor, fn_reference, output_fname,
+             output_dir=None,
+             desc=None):
     """
     Tests a SLU DAILogRegClassifier model.
 
-    :param fn_model:
     :param fn_input:
     :param constructor:
     :param fn_reference:
+    :param output_fname:
+    :param output_dir: If provided, specifies path towards the directory where
+        the output shall be written. If not specified, output is written to the
+        same directory as where the input files are read from.
+    :param desc: If provided, gives the description of the SLU -- to be logged.
     :return:
-    """
-    print "=" * 120
-    print "Testing: ", fn_model, fn_input, fn_reference
-    print "-" * 120
 
-    from alex.applications.PublicTransportInfoCS.preprocessing import PTICSSLUPreprocessing
-    from alex.components.slu.base import CategoryLabelDatabase
-    from alex.components.slu.dailrclassifier import DAILogRegClassifier
+    """
+
     from alex.corpustools.wavaskey import load_wavaskey, save_wavaskey
     from alex.corpustools.semscore import score
 
-    cldb = CategoryLabelDatabase(CLDB_PATH)
-    preprocessing = PTICSSLUPreprocessing(cldb)
-    slu = DAILogRegClassifier(cldb, preprocessing)
+    # Interpret arguments.
+    if output_dir is None:
+        output_dir = dirname(fn_input)
 
-    slu.load_model(fn_model)
+    # Prepare for decoding.
+    testing_msg = ('Testing {desc}'.format(desc=desc) if desc is not None
+                   else 'Testing SLU')
+    print "=" * 60
+    print testing_msg
+    print "-" * 60
 
+    # Load the data.
     test_utterances = load_wavaskey(fn_input, constructor, limit=100000)
 
+    # Decode the data.
     parsed_das = {}
     for utt_key, utt in sorted(test_utterances.iteritems()):
         if isinstance(utt, Utterance):
@@ -66,9 +76,9 @@ def trained_slu_test(fn_model, fn_input, constructor, fn_reference):
         elif isinstance(utt, UtteranceNBList):
             obs = {'utt_nbl': utt}
         else:
-            raise BaseException('Unsupported observation type')
+            raise Exception('Unsupported observation type')
 
-        print '-' * 120
+        print '-' * 60
         print "Observation:"
         print utt_key, " ==> "
         print unicode(utt)
@@ -87,28 +97,25 @@ def trained_slu_test(fn_model, fn_input, constructor, fn_reference):
         parsed_das[utt_key] = dah.da
 
         if 'CL_' in str(dah.da):
-            print '*' * 120
+            print '*' * 60
             print utt
             print dah.da
             slu.parse(obs, verbose=True)
 
-    if 'trn' in fn_model:
-        fn_sem = basename(fn_input) + '.model.trn.sem.out'
-    elif 'asr' in fn_model:
-        fn_sem = basename(fn_input) + '.model.asr.sem.out'
-    elif 'nbl' in fn_model:
-        fn_sem = basename(fn_input) + '.model.nbl.sem.out'
-    else:
-        fn_sem = basename(fn_input) + '.XXX.sem.out'
+    # Write the parses.
+    parses_out_fpath = join(output_dir, output_fname)
+    save_wavaskey(parses_out_fpath, parsed_das, trans=sort_dais)
 
-    save_wavaskey(fn_sem, parsed_das, trans=sort_dais)
+    # Determine where the score should be written.
+    scores_fpath = '{outpath}.score'.format(outpath=parses_out_fpath)
 
-    f = codecs.open(basename(fn_sem) + '.score', 'w+', encoding='UTF-8')
-    score(fn_reference, fn_sem, True, True, f)
-    f.close()
+    # Write the score.
+    with codecs.open(scores_fpath, 'w', encoding='UTF-8') as score_file:
+        score(fn_reference, parses_out_fpath, True, True, score_file)
 
 
-def hdc_slu_test(fn_input, constructor, fn_reference):
+def trained_slu_test(fn_model, fn_input, constructor, fn_reference,
+                     output_dir=None):
     """
     Tests a SLU DAILogRegClassifier model.
 
@@ -116,64 +123,111 @@ def hdc_slu_test(fn_input, constructor, fn_reference):
     :param fn_input:
     :param constructor:
     :param fn_reference:
+    :param output_dir: If provided, specifies path towards the directory where
+        the output shall be written. If not specified, output is written to the
+        same directory as where the input files are read from.
     :return:
-    """
-    print "=" * 120
-    print "Testing HDC SLU: ", fn_input, fn_reference
-    print "-" * 120
 
-    from alex.components.slu.base import CategoryLabelDatabase
-    from alex.applications.PublicTransportInfoCS.preprocessing import PTICSSLUPreprocessing
-    from alex.applications.PublicTransportInfoCS.hdc_slu import PTICSHDCSLU
-    from alex.corpustools.wavaskey import load_wavaskey, save_wavaskey
-    from alex.corpustools.semscore import score
+    """
+
+    # Construct the SLU.
+    from alex.components.slu.dailrclassifier import DAILogRegClassifier
 
     cldb = CategoryLabelDatabase(CLDB_PATH)
     preprocessing = PTICSSLUPreprocessing(cldb)
-    hdc_slu = PTICSHDCSLU(preprocessing)
+    slu = DAILogRegClassifier(cldb, preprocessing)
 
-    test_utterances = load_wavaskey(fn_input, constructor, limit=100000)
+    slu.load_model(fn_model)
 
-    parsed_das = {}
-    for utt_key, utt in sorted(test_utterances.iteritems()):
-        if isinstance(utt, Utterance):
-            obs = {'utt': utt}
-        elif isinstance(utt, UtteranceNBList):
-            obs = {'utt_nbl': utt}
-        else:
-            raise BaseException('Unsupported observation type')
+    # Construct the SLU description.
+    desc = 'log-reg SLU: {}'.format(' '.join((fn_model,
+                                              fn_input,
+                                              fn_reference)))
 
-        print '-' * 120
-        print "Observation:"
-        print utt_key, " ==> "
-        print unicode(utt)
+    # Determine the output filename.
+    if 'trn' in fn_model:
+        extension = 'model.trn.sem.out'
+    elif 'asr' in fn_model:
+        extension = 'model.asr.sem.out'
+    elif 'nbl' in fn_model:
+        extension = 'model.nbl.sem.out'
+    else:
+        extension = 'XXX.sem.out'
+    output_fname = '{bn}.{ext}'.format(bn=basename(fn_input),
+                                       ext=extension)
 
-        da_confnet = hdc_slu.parse(obs, verbose=False)
+    test_slu(slu, fn_input, constructor, fn_reference, output_fname,
+             output_dir, desc)
 
-        print "Conf net:"
-        print unicode(da_confnet)
 
-        da_confnet.prune()
-        dah = da_confnet.get_best_da_hyp()
+def hdc_slu_test(fn_input, constructor, fn_reference, output_dir=None):
+    """
+    Tests a SLU DAILogRegClassifier model.
 
-        print "1 best: "
-        print unicode(dah)
+    :param fn_input:
+    :param constructor:
+    :param fn_reference:
+    :param output_dir: If provided, specifies path towards the directory where
+        the output shall be written. If not specified, output is written to the
+        same directory as where the input files are read from.
+    :return:
 
-        parsed_das[utt_key] = dah.da
+    """
 
-        if 'CL_' in str(dah.da):
-            print '*' * 120
-            print utt
-            print dah.da
-            hdc_slu.parse(obs, verbose=True)
+    # Construct the SLU.
+    from alex.applications.PublicTransportInfoCS.hdc_slu import PTICSHDCSLU
 
-    fn_sem = basename(fn_input) + '.hdc.slu.sem.out'
+    cldb = CategoryLabelDatabase(CLDB_PATH)
+    preprocessing = PTICSSLUPreprocessing(cldb)
+    slu = PTICSHDCSLU(preprocessing)
 
-    save_wavaskey(fn_sem, parsed_das, trans=sort_dais)
+    # Construct the SLU description.
+    desc = 'HDC SLU: {}'.format(' '.join((fn_input, fn_reference)))
 
-    f = codecs.open(basename(fn_sem) + '.score', 'w+', encoding='UTF-8')
-    score(fn_reference, fn_sem, True, True, f)
-    f.close()
+    # Determine the output filename.
+    output_fname = '{base}.hdc.slu.sem.out'.format(base=basename(fn_input))
+
+    test_slu(slu, fn_input, constructor, fn_reference, output_fname,
+             output_dir, desc)
+
+
+def eval_all_on_all(models_dir, data_dir, output_dir=None):
+    all_refs_fpath = join(data_dir, 'all.trn.hdc.sem')
+    for utthyp_type in ('trn', 'asr', 'nbl'):
+        all_utts_fpath = join(data_dir, 'all.{typ}'.format(typ=utthyp_type))
+        utthyp_constructor = constructor_for_utthyp(utthyp_type)
+
+        hdc_slu_test(all_utts_fpath, utthyp_constructor, all_refs_fpath,
+                     output_dir)
+
+        model_fname = get_model_fname('all', utthyp_type)
+        model_fpath = join(models_dir, model_fname)
+        trained_slu_test(model_fpath,
+                         all_utts_fpath,
+                         utthyp_constructor,
+                         all_refs_fpath,
+                         output_dir)
+
+
+def eval_crossparts(models_dir, data_dir, output_dir=None):
+    for part in PARTS:
+        refs_fname = '{part}.trn.hdc.sem'.format(part=part)
+        refs_fpath = join(data_dir, refs_fname)
+
+        for utthyp_type in ('trn', 'asr', 'nbl'):
+            model_fname = get_model_fname(part, utthyp_type)
+            model_fpath = join(models_dir, model_fname)
+
+            utts_fname = '{part}.{utthyp_type}'.format(**locals())
+            utts_fpath = join(data_dir, utts_fname)
+
+            utthyp_constructor = constructor_for_utthyp(utthyp_type)
+
+            trained_slu_test(model_fpath,
+                             utts_fpath,
+                             utthyp_constructor,
+                             refs_fpath,
+                             output_dir)
 
 
 def parse_args(argv=None):
@@ -201,53 +255,22 @@ def parse_args(argv=None):
     return args
 
 
-def eval_all_on_all(models_dir, data_dir):
-    all_refs_fpath = join(data_dir, 'all.trn.hdc.sem')
-    for utthyp_type in ('trn', 'asr', 'nbl'):
-        all_utts_fpath = join(data_dir, 'all.{typ}'.format(typ=utthyp_type))
-        utthyp_constructor = constructor_for_utthyp(utthyp_type)
-
-        hdc_slu_test(all_utts_fpath, utthyp_constructor, all_refs_fpath)
-
-        model_fname = get_model_fname('all', utthyp_type)
-        model_fpath = join(models_dir, model_fname)
-        trained_slu_test(model_fpath,
-                         all_utts_fpath,
-                         utthyp_constructor,
-                         all_refs_fpath)
-
-
-def eval_crossparts(models_dir, data_dir):
-    for part in PARTS:
-        refs_fname = '{part}.trn.hdc.sem'.format(part=part)
-        refs_fpath = join(data_dir, refs_fname)
-
-        for utthyp_type in ('trn', 'asr', 'nbl'):
-            model_fname = get_model_fname(part, utthyp_type)
-            model_fpath = join(models_dir, model_fname)
-
-            utts_fname = '{part}.{utthyp_type}'.format(**locals())
-            utts_fpath = join(data_dir, utts_fname)
-
-            utthyp_constructor = constructor_for_utthyp(utthyp_type)
-
-            trained_slu_test(model_fpath, utts_fpath, utthyp_constructor,
-                             refs_fpath)
-
-
 def main(argv=None):
     args = parse_args(argv)
 
     # CHEATING: experiment on all data using models trained on all data
     for data_dir in args.input_dirs:
-        eval_all_on_all(args.models_dir, data_dir)
+        eval_all_on_all(args.models_dir, data_dir, args.output_dir)
 
     # REGULAR EXPERIMENT: evaluating models trained on training data and evaluated on deb and test data
     # **WARNING** due to data sparsity the metrics on the dev and test data fluctuate a lot
     # therefore meaning full results can be only obtained using N-fold cross validation
     for data_dir in args.input_dirs:
-        eval_crossparts(args.models_dir, data_dir)
+        eval_crossparts(args.models_dir, data_dir, args.output_dir)
 
 
 if __name__ == "__main__":
     main()
+
+
+# pymode:lint_ignore=E501,E221:
